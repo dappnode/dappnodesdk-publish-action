f19e6a2fa029b1159bfd1d5c05d11b84d5cc0516
dapplion <guillem.lion.benosa@gmail.com>
2020-10-19 12:55:16 +0200

    Test properly

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,7 +1,6 @@
 import * as process from 'process'
 import * as cp from 'child_process'
 import * as path from 'path'
-import {dappnodesdk} from '@dappnode/dappnodesdk'
 
 test('TODO', async () => {
   expect(true).toBeTruthy()
@@ -11,18 +10,11 @@ test('TODO', async () => {
 test('test runs', async () => {
   const testDnpDirectory = './test-dnp'
 
-  // Initialize a lightweight DNP in this directory
-  const manifest = await dappnodesdk.init({
-    yes: true,
-    force: true,
-    dir: testDnpDirectory
-  })
-  console.log(`Initialized test DNP: ${manifest.name} ${manifest.version}`)
-
   process.env['INPUT_DEVELOPER_ADDRESS'] =
     '0xf35960302a07022aba880dffaec2fdd64d5bf1c1'
   process.env['INPUT_DAPPNODE_TEAM_PRESET'] = 'false'
   process.env['INPUT_DIR'] = testDnpDirectory
+  process.env['TESTING_ONLY_INIT_FIRST'] = 'true'
 
   const ip = path.join(__dirname, '..', 'lib', 'main.js')
   const options: cp.ExecSyncOptions = {
--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,21 @@ import * as core from '@actions/core'
 import {dappnodesdk} from '@dappnode/dappnodesdk'
 
 async function run(): Promise<void> {
+  if (process.env.TESTING_ONLY_INIT_FIRST) {
+    await dappnodesdk.init({
+      yes: true,
+      force: true,
+      dir: core.getInput('dir') || './'
+    })
+  }
+
   const {releaseMultiHash} = await dappnodesdk.publish({
-    type: process.env.RELEASE_TYPE ?? 'patch',
-    dir: core.getInput('dir'),
+    type: process.env.RELEASE_TYPE || 'patch',
+    dir: core.getInput('dir') || './',
     dappnode_team_preset: toBoolean(core.getInput('dappnode_team_preset')),
-    eth_provider: core.getInput('eth_provider'),
-    content_provider: core.getInput('content_provider'),
-    timeout: core.getInput('timeout'),
+    eth_provider: core.getInput('eth_provider') || 'remote',
+    content_provider: core.getInput('content_provider') || 'remote',
+    timeout: core.getInput('timeout') || '60min',
     upload_to: 'ipfs',
     developer_address: core.getInput('developer_address')
   })
@@ -18,9 +26,9 @@ async function run(): Promise<void> {
 }
 
 function toBoolean(s: string | undefined): boolean | undefined {
-  if (s === undefined) return undefined
   if (s === 'false' || s === 'FALSE') return false
-  else return true
+  else if (s) return true
+  else return undefined
 }
 
 // eslint-disable-next-line github/no-then
