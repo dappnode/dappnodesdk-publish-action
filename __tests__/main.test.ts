f282cca87b0ed6cb0b1d2f37c6fa764f847e2f8d
dapplion <guillem.lion.benosa@gmail.com>
2020-10-19 12:27:15 +0200

    Test on different folder

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -9,13 +9,20 @@ test('TODO', async () => {
 
 // shows how the runner will run a javascript action with env / stdout protocol
 test('test runs', async () => {
+  const testDnpDirectory = './test-dnp'
+
   // Initialize a lightweight DNP in this directory
-  const manifest = await dappnodesdk.init({yes: true, force: true, dir: './'})
+  const manifest = await dappnodesdk.init({
+    yes: true,
+    force: true,
+    dir: testDnpDirectory
+  })
   console.log(`Initialized test DNP: ${manifest.name} ${manifest.version}`)
 
   process.env['DEVELOPER_ADDRESS'] =
     '0xf35960302a07022aba880dffaec2fdd64d5bf1c1'
   process.env['DAPPNODE_TEAM_PRESET'] = 'false'
+  process.env['RELEASE_DIR'] = testDnpDirectory
 
   const ip = path.join(__dirname, '..', 'lib', 'main.js')
   const options: cp.ExecSyncOptions = {
