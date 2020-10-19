2190c2085388756273430cef3c2c80d78b13f6a7
dapplion <guillem.lion.benosa@gmail.com>
2020-10-19 12:21:18 +0200

    Setup action

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,17 +1,30 @@
 import * as process from 'process'
 import * as cp from 'child_process'
 import * as path from 'path'
+import {dappnodesdk} from '@dappnode/dappnodesdk'
 
 test('TODO', async () => {
   expect(true).toBeTruthy()
 })
 
 // shows how the runner will run a javascript action with env / stdout protocol
-// test('test runs', () => {
-//   process.env['INPUT_MILLISECONDS'] = '500'
-//   const ip = path.join(__dirname, '..', 'lib', 'main.js')
-//   const options: cp.ExecSyncOptions = {
-//     env: process.env
-//   }
-//   console.log(cp.execSync(`node ${ip}`, options).toString())
-// })
+test('test runs', async () => {
+  // Initialize a lightweight DNP in this directory
+  const manifest = await dappnodesdk.init({yes: true, force: true, dir: './'})
+  console.log(`Initialized test DNP: ${manifest.name} ${manifest.version}`)
+
+  process.env['DEVELOPER_ADDRESS'] =
+    '0xf35960302a07022aba880dffaec2fdd64d5bf1c1'
+  process.env['DAPPNODE_TEAM_PRESET'] = 'false'
+
+  const ip = path.join(__dirname, '..', 'lib', 'main.js')
+  const options: cp.ExecSyncOptions = {
+    env: process.env
+  }
+  try {
+    console.log(cp.execSync(`node ${ip}`, options).toString())
+  } catch (e) {
+    console.error(`Error running main.js\n${e.stdout.toString()}`)
+    throw e
+  }
+})
--- src/main.ts
+++ src/main.ts
@@ -2,28 +2,44 @@ import * as core from '@actions/core'
 import {dappnodesdk} from '@dappnode/dappnodesdk'
 
 async function run(): Promise<void> {
-  try {
-    const developer_address: string = core.getInput('developer_address')
-    if (developer_address)
-      core.info(`Set developer_address: ${developer_address}`)
+  const developer_address: string = core.getInput('developer_address')
+  if (developer_address)
+    core.info(`Set developer_address: ${developer_address}`)
 
-    const {releaseMultiHash} = await dappnodesdk.publish({
-      type: 'patch',
-      dir: './',
-      dappnode_team_preset: true,
-      eth_provider: 'remote',
-      content_provider: 'remote',
-      timeout: '60min',
-      upload_to: 'ipfs',
-      developer_address
-    })
+  const {
+    RELEASE_TYPE,
+    RELEASE_DIR,
+    DAPPNODE_TEAM_PRESET,
+    ETH_PROVIDER,
+    CONTENT_PROVIDER,
+    UPLOAD_TO,
+    BUILD_TIMEOUT,
+    DEVELOPER_ADDRESS
+  } = process.env
 
-    core.info(`Release multihash: ${releaseMultiHash}`)
-    core.setOutput('releaseMultiHash', releaseMultiHash)
-  } catch (error) {
-    core.error(error.stack)
-    core.setFailed(error.message)
-  }
+  const {releaseMultiHash} = await dappnodesdk.publish({
+    type: RELEASE_TYPE ?? 'patch',
+    dir: RELEASE_DIR ?? './',
+    dappnode_team_preset: toBoolean(DAPPNODE_TEAM_PRESET) ?? true,
+    eth_provider: ETH_PROVIDER ?? 'remote',
+    content_provider: CONTENT_PROVIDER ?? 'remote',
+    timeout: BUILD_TIMEOUT ?? '60min',
+    upload_to: UPLOAD_TO ?? 'ipfs',
+    developer_address: DEVELOPER_ADDRESS
+  })
+
+  core.info(`Release multihash: ${releaseMultiHash}`)
+  core.setOutput('releaseMultiHash', releaseMultiHash)
 }
 
-run()
+function toBoolean(s: string | undefined): boolean | undefined {
+  if (s === undefined) return undefined
+  if (s === 'false' || s === 'FALSE') return false
+  else return true
+}
+
+// eslint-disable-next-line github/no-then
+run().catch(error => {
+  core.error(error.stack)
+  core.setFailed(error.message)
+})
