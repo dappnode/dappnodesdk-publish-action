0eed2d596dc197485123bb6abdcfd00d64dc7c2c
dapplion <guillem.lion.benosa@gmail.com>
2020-10-19 11:36:21 +0200

    Skip placeholder tests

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -7,11 +7,11 @@ test('TODO', async () => {
 })
 
 // shows how the runner will run a javascript action with env / stdout protocol
-test('test runs', () => {
-  process.env['INPUT_MILLISECONDS'] = '500'
-  const ip = path.join(__dirname, '..', 'lib', 'main.js')
-  const options: cp.ExecSyncOptions = {
-    env: process.env
-  }
-  console.log(cp.execSync(`node ${ip}`, options).toString())
-})
+// test('test runs', () => {
+//   process.env['INPUT_MILLISECONDS'] = '500'
+//   const ip = path.join(__dirname, '..', 'lib', 'main.js')
+//   const options: cp.ExecSyncOptions = {
+//     env: process.env
+//   }
+//   console.log(cp.execSync(`node ${ip}`, options).toString())
+// })
